b2ac6410e19e3ebe4e1708424cb2b4e16b5d43cc
Ross Landino <mrlandino@gmail.com>
2022-02-07 17:57:19 -0700

    Finish test for Unicorn and Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider, hungry = true, timesEaten = 0) {
+    this.name = name;
+    this.rider = rider;
+    this.greet = function greet() {
+      return (`Hi, ${this.rider}!`);
+    }
+    this.hungry = hungry;
+    this.timesEaten = timesEaten;
+    this.eat = function eat() {
+      if (this.timesEaten <= 2) {
+        this.timesEaten = this.timesEaten + 1;
+      }
+      if (this.timesEaten >= 3) {
+        this.hungry = false;
+      }
+    }
+  }
+}
+// learnings: add another property to store information in a variable to reference later via this.<new property>
+//not sure why I need another if statement instead of an else of if else statement?????
 
+module.exports = Dragon;
--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,20 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = "white") {
     this.name = name;
     this.color = color;
+    this.isWhite = function isWhite () {
+      if (this.color === "white") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    this.says = function says(says) {
+      return(`**;* ${says} *;**`);
+    }
   }
 }
 
+//learnings: you can set a default parameter with the parameter = whatever you want the default to be.
+// there can be nothing in between the constructor and parameters. 
 module.exports = Unicorn;
