97c783dc54c81184dc55e510c6c6d37851df5db0
Ross Landino <mrlandino@gmail.com>
2022-02-16 09:35:10 -0700

    Complete first 2 tests for direwolf

---
+++ mythical-creatures/exercises/direwolf.js
@@ -0,0 +1,10 @@
+class Direwolf{
+  constructor(name, home, size){
+    this.name = name;
+    this.home = home || "Beyond the Wall";
+    this.size = size || "Massive";
+  }
+}
+
+
+module.exports = Direwolf;
